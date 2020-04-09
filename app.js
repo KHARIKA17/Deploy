8e5d6f2aa3c6bab51f991abb65475f57c8b8308c
Gundu Pooja <gundupooja1995@gmail.com>
2020-04-09 13:41:08 -0500

    edited app.js and added models folder with 4 files in it.

--- app.js
+++ app.js
@@ -8,10 +8,17 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+//Set up mongoose connection
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb+srv://poojagundu:<Pooja31095$>@cluster0-yaqki.mongodb.net/local_library?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true ,useUnifiedTopology:true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 
 app.use(logger('dev'));
 app.use(express.json());
