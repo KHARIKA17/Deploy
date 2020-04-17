c236f24139023acd109730a6e686ab6122c30b1f
Gundu Pooja <gundupooja1995@gmail.com>
2020-04-17 11:29:59 -0500

    modified app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-// const  dotenv = require('dotenv')
+ const  dotenv = require('dotenv')
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
