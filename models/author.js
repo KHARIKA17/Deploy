8efc08f634d0ac2fcc594d079ae4131eaa962a33
Gundu Pooja <gundupooja1995@gmail.com>
2020-04-24 13:31:31 -0500

    checked code

--- models/author.js
+++ models/author.js
@@ -1,7 +1,8 @@
-let mongoose = require('mongoose');
-let moment = require('moment'); // For date handling.
+  
+const mongoose = require('mongoose');
+const moment = require('moment'); // For date handling.
 
-let Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const AuthorSchema = new Schema({
   first_name: { type: String, required: true, max: 100 },
@@ -12,7 +13,7 @@ const AuthorSchema = new Schema({
 
 // Virtual for author "full" name.
 AuthorSchema.virtual('name').get(function() {
-  const fullname = '';
+  let fullname = '';
 
   if (this.first_name && this.family_name) {
     fullname = this.family_name + ', ' + this.first_name;
@@ -30,7 +31,7 @@ AuthorSchema.virtual('url').get(function() {
 });
 
 AuthorSchema.virtual('lifespan').get(function() {
-  const lifetime_string = '';
+  let lifetime_string = '';
   if (this.date_of_birth) {
     lifetime_string = moment(this.date_of_birth).format('MMMM Do, YYYY');
   }
